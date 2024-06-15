8207b6f07d1e76849c7ff5ed19808a2970458c61
Anthony DEGROOTE <anthony.degroote@viacesi.fr>
2024-06-15 09:47:36 +0200

    Green : Add TestC,  Print of C

---
+++ DiamondTest/Diamond.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Text;
+
+namespace DiamondTest;
+
+internal static class Diamond
+{
+
+    internal static string Print(char v)
+    {
+        StringBuilder stringBuilder = new();
+
+        stringBuilder.AppendLine("  A  ".TrimEnd()); 
+        stringBuilder.AppendLine(" B B ".TrimEnd());
+        stringBuilder.AppendLine("C   C".TrimEnd()); 
+        stringBuilder.AppendLine(" B B ".TrimEnd());
+        stringBuilder.Append("  A  ".TrimEnd());
+
+        return stringBuilder.ToString();
+    }
+}
--- DiamondTest/UnitTest1.cs
+++ DiamondTest/UnitTest1.cs
@@ -3,8 +3,22 @@ namespace DiamondTest;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void TestC()
     {
+        // Arrange
+        string expected =
+            """
+              A
+             B B
+            C   C
+             B B
+              A
+            """;
 
+        // Act
+        string actual = Diamond.Print('C');
+
+        // Assert
+        Assert.Equal(expected, actual);
     }
 }
