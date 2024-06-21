81da08f0ca19df07addc6a191473cfdd287e167c
Anthony DEGROOTE <anthony.degroote@viacesi.fr>
2024-06-21 13:36:36 +0200

    Add loop for demonstration

--- KataDiamond/Program.cs
+++ KataDiamond/Program.cs
@@ -10,7 +10,7 @@ do
         Thread.Sleep(sleepTime);
         Console.Clear();
     }
-    for (char letter = 'Z'; letter >= 'B'; letter--)
+    for (char letter = 'Z'; letter > 'A'; letter--)
     {
         Console.WriteLine(Diamond.Print(letter));
         Thread.Sleep(sleepTime);
