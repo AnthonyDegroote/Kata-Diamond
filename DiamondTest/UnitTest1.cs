5035539b7de61e2bda52467ab9ff71bcfc6b9688
Anthony DEGROOTE <anthony.degroote@viacesi.fr>
2024-06-21 10:42:55 +0200

    Refactoring : Clean code

--- DiamondTest/UnitTest1.cs
+++ DiamondTest/UnitTest1.cs
@@ -24,7 +24,7 @@ public class UnitTest1
 
     [Theory(DisplayName = "TestLineMiddleIndexTwo")]
     // Arrange
-    [InlineData('E', 2,  "  C     C")]        // EDCBABCDE
+    [InlineData('E', 2,   "  C     C")]       // EDCBABCDE
     [InlineData('F', 2,  "  D       D")]     // FEDCBABCDEF
     [InlineData('G', 2, "  E         E")]   // GFEDCBABCDEFG
     public void TestLineMiddleIndexTwo(char c, uint indexOfMiddle, string exceptedLineMax)
